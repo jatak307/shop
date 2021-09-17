5f4524eb4409220e6478da55f75a0876582bf958
jatak307 <jatak307@gmail.com>
2021-09-17 17:59:45 +0300

    feat: add blocks to the header

--- shop/src/app/core/core.module.ts
+++ shop/src/app/core/core.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 import { HeaderComponent } from './components/header/header.component';
+import { HeaderInfoComponent } from './components/header-info/header-info.component';
+import { HeaderNavComponent } from './components/header-nav/header-nav.component';
+import { HeaderCategoriesComponent } from './components/header-categories/header-categories.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    HeaderInfoComponent,
+    HeaderNavComponent,
+    HeaderCategoriesComponent
   ],
   imports: [
     SharedModule
