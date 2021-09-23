ecd5b4555f989f76c7f66601d74066b67c9f25bc
jatak307 <jatak307@gmail.com>
2021-09-23 18:37:39 +0300

    feat: add pages category and detailed

--- shop/src/app/shop/shop.module.ts
+++ shop/src/app/shop/shop.module.ts
@@ -5,9 +5,11 @@ import { PopularGoodsComponent } from './components/popular-goods/popular-goods.
 import { SliderComponent } from './components/slider/slider.component';
 import { MainComponent } from './pages/main/main.component';
 import { ShopRoutingModule } from './shop-routing/shop-routing.module';
+import { CategoryComponent } from './pages/category/category.component';
+import { DetailedPageComponent } from './pages/detailed-page/detailed-page.component';
 
 @NgModule({
-  declarations: [SliderComponent, PopularGoodsComponent, MainComponent],
+  declarations: [SliderComponent, PopularGoodsComponent, MainComponent, CategoryComponent, DetailedPageComponent],
   imports: [SharedModule, ShopRoutingModule],
 })
 export class ShopModule {}
