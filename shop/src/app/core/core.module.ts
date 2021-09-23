8a6437557738153b970139767610643fd9ca9beb
jatak307 <jatak307@gmail.com>
2021-09-23 23:13:31 +0300

    feat: add footer component

--- shop/src/app/core/core.module.ts
+++ shop/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { HeaderCategoriesComponent } from './components/header-categories/header
 import { HeaderInfoComponent } from './components/header-info/header-info.component';
 import { HeaderNavComponent } from './components/header-nav/header-nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderInfoComponent,
     HeaderNavComponent,
     HeaderCategoriesComponent,
+    FooterComponent,
   ],
   imports: [SharedModule, HttpClientModule],
   exports: [HeaderComponent],
