62069b558a0d7aae0b8c5ad9e5f233cd0b631561
jatak307 <jatak307@gmail.com>
2021-09-25 02:45:53 +0300

    feat: display the product on the detailed info page

--- shop/src/app/shop/components/detailed-info/detailed-info.component.ts
+++ shop/src/app/shop/components/detailed-info/detailed-info.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Product } from 'src/app/core/models/product.model';
 
 @Component({
   selector: 'app-detailed-info',
   templateUrl: './detailed-info.component.html',
   styleUrls: ['./detailed-info.component.scss'],
 })
-export class DetailedInfoComponent {}
+export class DetailedInfoComponent {
+  @Input() product?: Product | null;
+}
--- shop/src/app/shop/pages/detailed-page/detailed-page.component.ts
+++ shop/src/app/shop/pages/detailed-page/detailed-page.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { Product } from 'src/app/core/models/product.model';
+import { ProductService } from 'src/app/core/services/product/product.service';
 
 @Component({
   selector: 'app-detailed-page',
   templateUrl: './detailed-page.component.html',
   styleUrls: ['./detailed-page.component.scss'],
 })
-export class DetailedPageComponent {}
+export class DetailedPageComponent {
+  public product$ = new Subject<Product>();
+
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private productService: ProductService,
+  ) {}
+
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe((par) => {
+      this.productService.getItemById(par.itemId).subscribe((res) => {
+        this.product$.next(res);
+      });
+    });
+  }
+}
